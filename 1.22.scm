9d864519c5b83fa77974df7955a91c5ed80d5ea7
tjouni <42916201+tjouni@users.noreply.github.com>
2018-12-25 11:19:33 +0200

    ex 1.22 - testing primes time complexity

---
+++ 1.22.scm
@@ -0,0 +1,44 @@
+;;;; ex 1.22 - finding primes time complexity test
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square x)
+  (* x x))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes lower upper)
+  (cond ((= (remainder lower 2) 0) (search-for-primes (+ 1 lower)
+						      upper))
+	((<= lower upper) (timed-prime-test lower)
+	 (search-for-primes (+ 1 lower)
+			    upper))
+	(else lower)))
+
+(search-for-primes 1000000000 1000000021); .03 - .04
+(search-for-primes 10000000000 10000000061); .10 - .11
+(search-for-primes 100000000000 100000000057); .32 - .33
+(search-for-primes 1000000000000 1000000000063); .99 - 1.01
