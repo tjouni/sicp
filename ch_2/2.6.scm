a830c9be5f9ee810696fe5f05c9bf343f722e38c
tjouni <42916201+tjouni@users.noreply.github.com>
2019-01-03 13:23:32 +0200

    add 2.6

---
+++ ch_2/2.6.scm
@@ -0,0 +1,16 @@
+;;;; ex 2.6 - church numerals
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add n m)
+  (lambda (f) (lambda (x) ((n f) ((m f) x)))))
+
+((one square) 5);Value: 25
+(((add one two) square) 2);Value: 256
