0b8c98639c9a0b27319fd80cc366771b4c4bcf2b
tjouni <42916201+tjouni@users.noreply.github.com>
2019-01-05 15:04:07 +0200

    add 2.31

---
+++ ch_2/2.31.scm
@@ -0,0 +1,15 @@
+;;;; ex 2.31 - tree map
+
+(define (tree-map proc tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (proc tree))
+	(else (cons (tree-map proc (car tree))
+		    (tree-map proc (cdr tree))))))
+
+(define (square-tree tree) (tree-map square tree))
+
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)));Value 104: (1 (4 (9 16) 25) (36 49))
