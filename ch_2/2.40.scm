b792ad3f703731d19c0cb2fad11fb5ea1e0a5a50
tjouni <42916201+tjouni@users.noreply.github.com>
2019-01-14 21:38:12 +0200

    add 2.40

---
+++ ch_2/2.40.scm
@@ -0,0 +1,48 @@
+;;;; ex 2.40 - unique pairs
+
+(define nil '())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+	     (map (lambda (j) (list i j))
+		  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(unique-pairs 3);Value 12: ((2 1) (3 1) (3 2))
+(prime-sum-pairs 5);Value 13: ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7))
